b6fece9b8037e18d8bf25ab4c8f8a09a9ae04e29
totoalpina <totoalpina@github.com>
2022-02-22 08:35:48 +0200

    Refactor SQL scripts

--- SQLScripts/databaseScript.sql
+++ SQLScripts/databaseScript.sql
@@ -1,25 +1,20 @@
-create database password_organiser;
-use password_organiser;
+CREATE DATABASE password_organiser;
 
-create table conturi
-(
-    id          bigint       not null,
-    categorie   varchar(50)  null,
-    username    varchar(50)  null,
-    password    varchar(50)  null,
-    description varchar(255) null,
-    Primary key (id);	
-    constraint conturi_id_uindex
-        unique (id)
-);
+CREATE TABLE conturi (
+  id bigint(20) NOT NULL,
+  categorie varchar(50) DEFAULT NULL,
+  username varchar(50) DEFAULT NULL,
+  password varchar(50) DEFAULT NULL,
+  description varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY conturi_id_uindex (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE users (
+  id int(2) NOT NULL AUTO_INCREMENT,
+  username varchar(50) DEFAULT NULL,
+  password varchar(50) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY users_id_uindex (id)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-create table users
-(
-    id       int(2) auto_increment,
-    username varchar(50) null,
-    password varchar(50) null,
-    Primary key (id);	
-    constraint users_id_uindex
-        unique (id)
-);
